d390d83d8bed7a7f41e35531d0e006a8160b69c6
Carmen <carmen-m.98@gmx.de>
2024-11-01 15:15:06 +0100

    complete return of PlantCard

--- components/PlantCard/index.js
+++ components/PlantCard/index.js
@@ -5,11 +5,12 @@ export default function PlantCard() {
     return (
         <section>
             <Image
-                src={}
-                width={20}
-                height={20}
-                alt={} />
-            <p></p>
+                src={"https://upload.wikimedia.org/wikipedia/commons/4/45/Sansevieria_trifasciata_Laurentii_pm_4.jpg"}
+                width={200}
+                height={200}
+                alt={"Snake plant"} />
+            <h2>Snake plant</h2>
+            <h3>Sansevieria trifasciata</h3>
         </section>
     )
 }
