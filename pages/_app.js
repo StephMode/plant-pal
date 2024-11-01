f58e4f600554537d9bc6520e4116d49008a324ac
Carmen <carmen-m.98@gmx.de>
2024-11-01 14:53:17 +0100

    implement state initialPlants

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import GlobalStyle from "../styles";
 import plants from "@/lib/data";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [initialPlants, setInitialPlants] = useState(plants);
+
+  
   return (
     <>
       <GlobalStyle />
