16cbed8fe5657e034816e7a0b76ef436d9e8666e
Carmen <carmen-m.98@gmx.de>
2024-11-01 16:01:35 +0100

    add styling to PlantCard

--- components/PlantCard/index.js
+++ components/PlantCard/index.js
@@ -1,17 +1,35 @@
 import Image from "next/image"
+import styled from "styled-components"
 
 export default function PlantCard({ image, name, botanicalName }) {
 
     return (
         <section>
-            <Image
-                src={image}
-                width={200}
-                height={200}
-                alt={name} 
-            />
+            <StyledImageContainer>
+                <StyledImage
+                    src={image}
+                    alt={name} 
+                />
+            </StyledImageContainer>
             <h2>{name}</h2>
             <h3>{botanicalName}</h3>
         </section>
     )
 }
+
+
+
+
+const StyledImageContainer = styled.div`
+    width: 300px;
+    height: 300px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+`;
+
+const StyledImage = styled.img`
+    width: 200%;
+    height: auto;
+    text-align: center;
+`;
--- styles.js
+++ styles.js
@@ -1,6 +1,23 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
+  :root {
+    --gold: #FFBA00;
+    --gold-dark: #CE9605;
+    --green-main: #0C3B2E;
+    --green-main-dark: #08281F;
+    --green-light: #6D9773;
+    --green-light-dark: #4B684F;
+    --brown: #BB8A52;
+    --brown-dark: #86643E;
+    --white: #FBfBfB;
+    --white-dark: #cacaca;
+    --black: #010F0B;
+    --gray: #D2D5D2;
+    --gray-dark: #808080;
+  }
+
   *,
   *::before,
   *::after {
@@ -12,6 +29,8 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: system-ui;
+    display: flex;
+    justify-content: center;
   }
 
   ul, li {
