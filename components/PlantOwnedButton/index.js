c85974e6bb67965e38a07922fca00bb696d15f26
Stephan Model <stephan.model@outlook.de>
2024-11-04 12:37:02 +0100

    implement conditional return statement for PlantOwnedButton

--- components/PlantOwnedButton/index.js
+++ components/PlantOwnedButton/index.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
-export default function PlantOwnedButton({}) {
+export default function PlantOwnedButton({ isOwned, onClick }) {
   return (
-    <button type="button">
-      <Image src={"./heart.svg"} height={40} width={40} alt="heart icon" />
+    <button type="button" onClick={onClick}>
+      {isOwned ? (
+        <Image
+          src={"./heart-solid.svg"}
+          height={40}
+          width={40}
+          alt="heart icon"
+        />
+      ) : (
+        <Image src={"./heart.svg"} height={40} width={40} alt="heart icon" />
+      )}
     </button>
   );
 }
