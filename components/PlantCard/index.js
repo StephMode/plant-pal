b21e8532a38241648617ab65b421939569d248d9
jan-riemenschneider <jan-riemenschneider@outlook.de>
2024-11-04 14:42:47 +0100

    implement Link to PlantCard Component, drill prop id from Homepage to PlantCard

--- components/PlantCard/index.js
+++ components/PlantCard/index.js
@@ -1,33 +1,31 @@
-import Image from "next/image"
-import styled from "styled-components"
+import Image from "next/image";
+import styled from "styled-components";
+import Link from "next/link";
 
-export default function PlantCard({ image, name, botanicalName }) {
-
-    return (
-        <article>
-            <StyledImageContainer>
-                <StyledImage
-                    src={image}
-                    alt={name} 
-                />
-            </StyledImageContainer>
-            <h2>{name}</h2>
-            <h3>{botanicalName}</h3>
-        </article>
-    )
+export default function PlantCard({ image, name, botanicalName, id }) {
+  return (
+    <article>
+      <StyledImageContainer>
+        <StyledImage src={image} alt={name} />
+      </StyledImageContainer>
+      <Link href={`/${id}`}>
+        <h2>{name}</h2>
+        <h3>{botanicalName}</h3>
+      </Link>
+    </article>
+  );
 }
 
 const StyledImageContainer = styled.div`
-    width: 300px;
-    height: 300px;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
+  width: 300px;
+  height: 300px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
 `;
 
 const StyledImage = styled.img`
-    width: 200%;
-    height: auto;
-    text-align: center;
+  width: 200%;
+  height: auto;
+  text-align: center;
 `;
-
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,21 @@ import PlantCard from "@/components/PlantCard";
 import plants from "@/lib/data";
 
 export default function HomePage() {
-
   return (
     <>
       <main>
         <h1>Plant List</h1>
         <ul>
-          { plants.map((plant) => (
+          {plants.map((plant) => (
             <li key={plant.id}>
-              <PlantCard image={plant.imageUrl} name={plant.name} botanicalName={plant.botanicalName}/>
+              <PlantCard
+                image={plant.imageUrl}
+                name={plant.name}
+                botanicalName={plant.botanicalName}
+                id={plant.id}
+              />
             </li>
-          ))
-          }
+          ))}
         </ul>
       </main>
     </>
