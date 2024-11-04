91c80f956b82794fa92b129c6ab35f36ccfc5086
jan-riemenschneider <jan-riemenschneider@outlook.de>
2024-11-04 13:11:04 +0100

    implement return statement with needed plant information

--- pages/[id].js
+++ pages/[id].js
@@ -17,11 +17,20 @@ export default function PlantDetailsPage() {
 
   return (
     <>
-      <StyledImageContainer>
-        <StyledImage src={image} alt={name} />
-      </StyledImageContainer>
-      <h2>{name}</h2>
-      <h3>{botanicalName}</h3>
+      <main>
+        <h1>Plant Details</h1>
+        <h2>{plant.name}</h2>
+        <h3>{plant.botanicalName}</h3>
+        <Image src={plant.imageUrl} alt={plant.name} />
+        <p>{plant.lightNeed}</p>
+        <p>{plant.waterNeed}</p>
+        <ul>
+          {plant.fertiliserSeason.map((season) => (
+            <li key={season}>{season}</li>
+          ))}
+        </ul>
+        <p>{plant.description}</p>
+      </main>
     </>
   );
 }
