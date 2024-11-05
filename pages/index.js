49d462b6abe652b024cca0be5783128df4b71bce
Stephan Model <stephan.model@outlook.de>
2024-11-05 16:08:09 +0100

    clean up return statements

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,23 @@ import Link from "next/link";
 
 export default function HomePage({ handleToggleOwned, plants }) {
   return (
-    <>
-      <main>
-        <h1>Plant List</h1>
-        <ul>
-          {plants.map((plant) => (
-            <li key={plant.id}>
-              <PlantCard
-                plantId={plant.id}
-                image={plant.imageUrl}
-                name={plant.name}
-                botanicalName={plant.botanicalName}
-                handleToggleOwned={handleToggleOwned}
-                isOwned={plant.isOwned}
-              />
-            </li>
-          ))}
-        </ul>
-        <Link href={"/myplants"}>My Plants</Link>
-      </main>
-    </>
+    <main>
+      <h1>Plant List</h1>
+      <ul>
+        {plants.map((plant) => (
+          <li key={plant.id}>
+            <PlantCard
+              plantId={plant.id}
+              image={plant.imageUrl}
+              name={plant.name}
+              botanicalName={plant.botanicalName}
+              handleToggleOwned={handleToggleOwned}
+              isOwned={plant.isOwned}
+            />
+          </li>
+        ))}
+      </ul>
+      <Link href={"/myplants"}>My Plants</Link>
+    </main>
   );
 }
--- pages/myplants.js
+++ pages/myplants.js
@@ -6,29 +6,27 @@ export default function MyPlants({ handleToggleOwned, plants }) {
   const ownedPlants = plants.filter((plant) => plant.isOwned);
 
   return (
-    <>
-      <main>
-        <h1>My Plants</h1>
-        {ownedPlants.length === 0 && (
-          <StyledInfoText>Currently no plants in your list!</StyledInfoText>
-        )}
-        <ul>
-          {ownedPlants.map((plant) => (
-            <li key={plant.id}>
-              <PlantCard
-                plantId={plant.id}
-                image={plant.imageUrl}
-                name={plant.name}
-                botanicalName={plant.botanicalName}
-                handleToggleOwned={handleToggleOwned}
-                isOwned={plant.isOwned}
-              />
-            </li>
-          ))}
-        </ul>
-        <Link href={"/"}>Plant List</Link>
-      </main>
-    </>
+    <main>
+      <h1>My Plants</h1>
+      {ownedPlants.length === 0 && (
+        <StyledInfoText>Currently no plants in your list!</StyledInfoText>
+      )}
+      <ul>
+        {ownedPlants.map((plant) => (
+          <li key={plant.id}>
+            <PlantCard
+              plantId={plant.id}
+              image={plant.imageUrl}
+              name={plant.name}
+              botanicalName={plant.botanicalName}
+              handleToggleOwned={handleToggleOwned}
+              isOwned={plant.isOwned}
+            />
+          </li>
+        ))}
+      </ul>
+      <Link href={"/"}>Plant List</Link>
+    </main>
   );
 }
 
