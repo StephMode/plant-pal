07945d3ea2980a90e1a7c46f66272e1134d6618a
Carmen Müller <carmen-m.98@gmx.de>
2024-11-07 16:05:28 +0100

    Update index.js by adding fragments

--- pages/index.js
+++ pages/index.js
@@ -15,21 +15,23 @@ export default function HomePage({ handleToggleOwned, plants, handleAddPlant }) 
           <StyledButton type="button">Add Plant</StyledButton>
         </>
       ) : (
-      <AddPlantForm handleAddPlant={handleAddPlant}/>
-      <ul>
-        {plants.map((plant) => (
-          <li key={plant.id}>
-            <PlantCard
-              plantId={plant.id}
-              image={plant.imageUrl}
-              name={plant.name}
-              botanicalName={plant.botanicalName}
-              handleToggleOwned={handleToggleOwned}
-              isOwned={plant.isOwned}
-            />
-          </li>
-        ))}
-      </ul>
+        <>
+          <AddPlantForm handleAddPlant={handleAddPlant}/>
+          <ul>
+            {plants.map((plant) => (
+              <li key={plant.id}>
+                <PlantCard
+                  plantId={plant.id}
+                  image={plant.imageUrl}
+                  name={plant.name}
+                  botanicalName={plant.botanicalName}
+                  handleToggleOwned={handleToggleOwned}
+                  isOwned={plant.isOwned}
+                />
+              </li>
+            ))}
+          </ul>
+        </>
       )}
       <Link href="/myplants">My Plants</Link>
     </main>
