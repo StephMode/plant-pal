eb1a4a9d596f2aabdfd92c2cbca9964ee8d24014
Carmen <carmen-m.98@gmx.de>
2024-11-08 16:21:25 +0100

    Create Filter component

---
+++ components/PlantFilter/index.js
@@ -0,0 +1,17 @@
+
+
+export default function PlantFilter() {
+
+
+    return (
+        <>
+        <p>Filter options</p>
+            <button type="button">full sun</button>
+            <button type="button">partial shade</button>
+            <button type="button">full shade</button>
+            <button type="button">reset</button>
+        </>
+    )
+
+
+};
