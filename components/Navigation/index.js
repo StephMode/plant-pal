67b419ad56ab2610a50780220a899269ec06ff74
levin <levinthiel@proton.me>
2024-11-12 12:45:35 +0100

    styling the navigation bar and imported icons

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,9 +1,14 @@
 import styled from "styled-components"
+import { HiHome } from "react-icons/hi";
+import { FaPlus } from "react-icons/fa";
+import { IoHeart } from "react-icons/io5";
 
 export default function Navigation() { 
     return (
         <Stylednavcontainer>
-            <p>lalalalal</p>
+            <StyledIconContainer><HiHome /></StyledIconContainer>
+            <StyledIconContainer><FaPlus /></StyledIconContainer>
+            <StyledIconContainer><IoHeart /></StyledIconContainer>
         </Stylednavcontainer>
     )
 }
@@ -14,5 +19,24 @@ const Stylednavcontainer = styled.nav`
     width: 100%;
     height: 100px;
     background-color: var(--green-light);
-
+    border-radius: 20px 20px 0 0 ;
+    z-index: 999;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    box-shadow: 0 0px 37px rgba(0, 0, 0, 0.5);
 `;
+ const StyledIconContainer = styled.span `
+    background-color: var(--green-main);
+    border-radius: 40px;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--white);
+    font-size: 30px;
+
+    
+ `;
