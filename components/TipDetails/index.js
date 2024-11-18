d699522a36443f2e4d6d96585f1b017090816f11
Carmen <carmen-m.98@gmx.de>
2024-11-18 11:28:33 +0100

    clean up TipDetails component by removing unneccessary stylings

--- components/TipDetails/index.js
+++ components/TipDetails/index.js
@@ -11,27 +11,21 @@ export default function TipDetails({ tip }) {
             <StyledImage src={tip.imageURL} alt={tip.title} fill />
       </StyledImageContainer>
       
-      <StyledPlantContainer>
-      <StyledTopSection>
+      <StyledTipContainer>
         <StyledH2>{tip.title}</StyledH2>
-      </StyledTopSection>
-      <StyledH3>{tip.shortBodyContent}</StyledH3>
-      
-      <StyledDescription>{tip.bodyContent}</StyledDescription>
+        <StyledDescription>{tip.bodyContent}</StyledDescription>
+      </StyledTipContainer>
 
-      </StyledPlantContainer>
-
-
-        <Link href="/">
+      <Link href="/plantTipsPage">
         <StyledIconContainer>
           <FaChevronLeft />
         </StyledIconContainer>
-        </Link>
+      </Link>
     </>
   );
 }
 
-const StyledPlantContainer = styled.section`
+const StyledTipContainer = styled.section`
   display: block;
   margin: 0;
   width: 100%;
@@ -42,10 +36,6 @@ const StyledPlantContainer = styled.section`
   }
 `;
 
-const StyledPlantNeedsContainer = styled.article`
-  display: flex;
-  flex-direction: column;
-`;
 
 const StyledImageContainer = styled.div`
   width: 100%;
@@ -67,6 +57,7 @@ const StyledImage = styled(Image)`
   text-align: center;
   object-fit: cover;
 `;
+
 const StyledIconContainer = styled.span `
     background-color: var(--green-light);
     border-radius: 40px;
@@ -81,49 +72,13 @@ const StyledIconContainer = styled.span `
     top: 70px;
     left: 20px;
  `;
-const StyledIconSection = styled.section`
-  display: flex;
-  align-items: center;
-  color: var(--green-main);
-  font-weight: bold;
-  font-size: 20px;
-  gap: 10px;
-  margin-bottom: 5px;
-  flex-wrap: wrap;
-`;
- const StyledTopSection = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
- `;
+
  const StyledH2 = styled.h2`
-  margin-bottom: 2px;
+  margin-bottom: 13px;
   max-width: 260px;
  `;
- const StyledH3 = styled.h3`
-  margin-bottom: 15px;
-  font-style: italic;
-  font-weight: normal;
- `;
+
  const StyledDescription = styled.p`
   margin-bottom: 20px;
   text-align: justify;
  `;
- const StyledFertilizerUl = styled.ul`
-  gap: 5px;
-  margin : 0 0 0 30px;
-  justify-content: flex-start;
- `;
- const StyledFertilizerLi = styled.li`
-  color: var(--white);
-  background-color: var(--green-light);
-  padding: 3px 10px 5px 10px;
-  border-radius: 20px;
-  font-size: 14px;
-  font-weight: normal;
-`;
-const StyledEditDeleteSection = styled.section`
-  display: flex;
-  width: 100%;
-  margin-top: 25px;
-`;
