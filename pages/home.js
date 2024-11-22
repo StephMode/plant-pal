741de666ab5262d2fd7f3b350bd56c1cb2f7973e
levin <levinthiel@proton.me>
2024-11-22 09:11:03 +0100

    add automatic rerouting

---
+++ pages/home.js
@@ -0,0 +1,85 @@
+import PlantCard from "/components/PlantCard";
+import styled from "styled-components";
+import Button from "/components/Button";
+import PlantFilterSection from "/components/PlantFilterSection";
+import { AiOutlineControl } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+import TipBanner from "/components/TipBanner";
+import Link from "next/link";
+
+
+export default function HomePage({ 
+  handleToggleOwned, 
+  plants, 
+  onFilterPlants, 
+  showPlantFilterSection, 
+  toggleFilterSection, 
+  onFilterPlantsReset, 
+  selectedFilter, 
+  filteredPlants,
+  randomTip,
+  progress,
+
+  handleMouseLeave,
+  handleMouseHover
+}) {
+  const plantsToBeRendered = filteredPlants !== plants ? filteredPlants : plants;
+
+  return (
+    <main>
+      <h1>Plant List</h1>
+      <StyledSpacer/>
+      <TipBanner randomTip={randomTip} progress={progress} handleMouseHover = {handleMouseHover} handleMouseLeave = {handleMouseLeave}/>
+      <StyledFilterButtonSection>
+        <Button buttonText={showPlantFilterSection ? <IoClose /> : <AiOutlineControl />} handleButtonFunction={toggleFilterSection} />
+      </StyledFilterButtonSection>
+      <StyledSpacer2/>
+      <PlantFilterSection handleFilterPlants={onFilterPlants} showPlantFilterSection={showPlantFilterSection} handleFilterPlantsReset={onFilterPlantsReset} />
+      { filteredPlants.length === 0 && 
+           <StyledInfoText>No plants were found. Reset filter.</StyledInfoText>}
+      { !selectedFilter && plants.length === 0 ? (
+          <StyledInfoText>No plants there yet. Add new ones!</StyledInfoText>
+      ) : (
+          <ul>
+           {plantsToBeRendered.map((plant) => (
+              <li key={plant.id}>
+                <PlantCard
+                  plantId={plant.id}
+                  image={plant.imageUrl}
+                  name={plant.name}
+                  botanicalName={plant.botanicalName}
+                  handleToggleOwned={handleToggleOwned}
+                  isOwned={plant.isOwned}
+                />
+              </li>)
+            )}
+          </ul>
+      )}
+    </main>
+  );
+}
+
+
+const StyledInfoText = styled.p`
+  color: var(--green-main);
+  background-color: var(--gray);
+  margin: 10px 0;
+  padding: 40px;
+  border-radius: 25px;
+`;
+const StyledSpacer = styled.span`
+  display: block;
+  height: 75px;
+`;
+const StyledSpacer2 = styled.span`
+  display: block;
+  height: 17px;
+`;
+const StyledFilterButtonSection = styled.section`
+  margin-right: 6px;
+  align-self: end;
+`;
+
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,67 @@
-import PlantCard from "/components/PlantCard";
-import styled from "styled-components";
-import Button from "/components/Button";
-import PlantFilterSection from "/components/PlantFilterSection";
-import { AiOutlineControl } from "react-icons/ai";
-import { IoClose } from "react-icons/io5";
-import TipBanner from "/components/TipBanner";
-import Link from "next/link";
+import styled from "styled-components"
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
+export default function IntroPage() {
 
-export default function HomePage({ 
-  handleToggleOwned, 
-  plants, 
-  onFilterPlants, 
-  showPlantFilterSection, 
-  toggleFilterSection, 
-  onFilterPlantsReset, 
-  selectedFilter, 
-  filteredPlants,
-  randomTip,
-  progress,
+    const router = useRouter();
 
-  handleMouseLeave,
-  handleMouseHover
-}) {
-  const plantsToBeRendered = filteredPlants !== plants ? filteredPlants : plants;
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        router.push('/home');
+      }, 4500); 
+      return () => clearTimeout(timer); 
+    }, [router]);
 
-  return (
-    <main>
-      <h1>Plant List</h1>
-      <StyledSpacer/>
-      <Link href="/intro">intro page</Link>
-      <TipBanner randomTip={randomTip} progress={progress} handleMouseHover = {handleMouseHover} handleMouseLeave = {handleMouseLeave}/>
-      <StyledFilterButtonSection>
-        <Button buttonText={showPlantFilterSection ? <IoClose /> : <AiOutlineControl />} handleButtonFunction={toggleFilterSection} />
-      </StyledFilterButtonSection>
-      <StyledSpacer2/>
-      <PlantFilterSection handleFilterPlants={onFilterPlants} showPlantFilterSection={showPlantFilterSection} handleFilterPlantsReset={onFilterPlantsReset} />
-      { filteredPlants.length === 0 && 
-           <StyledInfoText>No plants were found. Reset filter.</StyledInfoText>}
-      { !selectedFilter && plants.length === 0 ? (
-          <StyledInfoText>No plants there yet. Add new ones!</StyledInfoText>
-      ) : (
-          <ul>
-           {plantsToBeRendered.map((plant) => (
-              <li key={plant.id}>
-                <PlantCard
-                  plantId={plant.id}
-                  image={plant.imageUrl}
-                  name={plant.name}
-                  botanicalName={plant.botanicalName}
-                  handleToggleOwned={handleToggleOwned}
-                  isOwned={plant.isOwned}
-                />
-              </li>)
-            )}
-          </ul>
-      )}
-    </main>
-  );
+    return(
+        <StyledIntroContainer>
+            <StyledImageContainer>
+                <Image src="/logo-main.svg" width={200} height={50} alt="rooted logo" />
+                <StyledBaseline>Master the art of plant care, one tip at a time.</StyledBaseline>
+            </StyledImageContainer>
+        </StyledIntroContainer>
+    )
 }
 
-
-const StyledInfoText = styled.p`
-  color: var(--green-main);
-  background-color: var(--gray);
-  margin: 10px 0;
-  padding: 40px;
-  border-radius: 25px;
+const StyledIntroContainer = styled.section`
+    background: no-repeat url(/intro.jpg) center center;
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+    background-size: cover;
+    z-index: 1000;
 `;
-const StyledSpacer = styled.span`
-  display: block;
-  height: 75px;
+const StyledImageContainer = styled.div`
+    top: 27%;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    background: var(--green-main);
+    width: 250px;
+    flex-direction: column;
+    color: var(--gold);
+    font-family: 'Assistant', sans serif;
+    padding: 30px;
+    border-radius: 35px;
+    animation-name: logo;
+    animation-duration: 5s;
+    transition: all cubic-bezier(0.65, 0.05, 0.36, 1);;
+
+@keyframes logo {
+  0%   {left:0px; top:0px; opacity:0;background: var(--green-light);}
+  10%  {opacity:0;}
+  30% {left:0px; top: 27%;opacity:1;background: var(--green-main);}
+  70% {left:0px; top: 27%;opacity:1;background: var(--green-main);}
+  100% {left:0px; top: 50%;opacity:0;}
+}
 `;
-const StyledSpacer2 = styled.span`
-  display: block;
-  height: 17px;
+const StyledBaseline = styled.p`
+    text-align: center;
+    margin-top: 10px;
 `;
-const StyledFilterButtonSection = styled.section`
-  margin-right: 6px;
-  align-self: end;
-`;
-
-
-
-
