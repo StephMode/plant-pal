abdff1026630fabd1849ed2ee8eeef9a75d13eca
levin <levinthiel@proton.me>
2024-11-22 08:30:11 +0100

    add background image

--- pages/intro.js
+++ pages/intro.js
@@ -8,5 +8,10 @@ export default function IntroPage() {
 }
 
 const StyledIntroContainer = styled.section`
-
+    background: no-repeat url(/intro.jpg) center center;
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+    background-size: cover;
 `;
