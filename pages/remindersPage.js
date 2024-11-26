8fe25fa84a0fb77ce4e4a70f87190b0313569739
Carmen <carmen-m.98@gmx.de>
2024-11-26 17:19:29 +0100

    implement back button on RemindersPage

--- pages/remindersPage.js
+++ pages/remindersPage.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 import ReminderCard from "/components/ReminderCard";
+import { FaChevronLeft } from "react-icons/fa6";
+import { useRouter } from "next/router";
 
 export default function RemindersPage({reminders, handleDeleteReminder}) {
+    const router = useRouter();
+
     return (
         <main>
             <h1>Reminders</h1>
+            <StyledIconContainer onClick={() =>  router.back()} type="button">
+          <FaChevronLeft />
+        </StyledIconContainer>
             <StyledSpacer />
             <ul>
             {reminders.length === 0 && <StyledInfoText>Currently no reminders.</StyledInfoText>}
@@ -22,7 +29,7 @@ export default function RemindersPage({reminders, handleDeleteReminder}) {
 
 const StyledSpacer = styled.span`
   display: block;
-  height: 113px;
+  height: 140px;
 `;
 
 const StyledInfoText = styled.p`
@@ -32,3 +39,19 @@ const StyledInfoText = styled.p`
   padding: 40px 40px;
   border-radius: 25px;
 `;
+
+const StyledIconContainer = styled.button `
+    background-color: var(--green-light);
+    border-radius: 40px;
+    border: none;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--white);
+    font-size: 30px;
+    position: absolute;
+    top: 70px;
+    left: 20px;
+ `;
