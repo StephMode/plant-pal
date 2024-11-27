710b087629236b9e55bdd9b6acbf79085b87b51c
Stephan Model <stephan.model@outlook.de>
2024-11-27 12:16:03 +0100

    add styling

--- components/Search/index.js
+++ components/Search/index.js
@@ -20,23 +20,39 @@ export default function Search({ handleSearchQuery, resetSearch, searchFor }) {
 
 
     return(
-        <>
+
             <StyledSearchBar>
-                <input 
+                <StyledSearchInput 
                 type="text"
                 name="searchQuery"
                 placeholder="Search for tip"
                 onChange={handleSearchInput}
-                />
+                ></StyledSearchInput>
             {/* hier kann ich auch gleich einen Reset Button "x" einbauen, der die Search reseted */}
             <IoSearch />
             </StyledSearchBar>
-        </>
     )
 }
 
 
 const StyledSearchBar = styled.div`
     display: flex;
+    align-items: center;
     margin: 20px;
+    gap: 5px;
+`;
+
+const StyledSearchInput = styled.input`
+  border: none;
+  border-radius: 30px;
+  padding: 10px 15px;
+  margin-top: 6px;
+  font-family: inherit;
+
+  background-color: rgba(0,0,0,0.1);
+  border: 2px solid rgba(0,0,0,0.1);
+
+  &:focus {
+    outline-color: var(--green-light);
+  }
 `;
